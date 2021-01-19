ab75f92bd53724f0869b026be83da47d08ea8b84
elliott lockwood <lockwood721@gmail.com>
2021-01-19 10:16:49 -0600

    final push

--- dml.sql
+++ dml.sql
@@ -1,2 +1,20 @@
-INSERT INTO employeetable (employee, empdetails, department)
-VALUES ("Tina Smith", "Marketing", )
+INSERT INTO employee (ID, firstname, lastname, ssn, deptid )
+VALUES ("1", "Tina" ,"Smith", "Marketing", "123", "12"  );
+VALUES ("1", "mike" ,"brown", "sales", "123", "12"  );
+VALUES ("1", "tom" ,"wilson", "hr", "123", "12"  )
+
+INSERT INTO empdetails (ID, employeeid, salary, address1, address2, city, state1, country )
+VALUES ("1", "123" ,"45000", "1234 street", "567 blvd", "dallas", "tx", "usa");
+VALUES ("1", "123" ,"45000", "1234 street", "567 blvd", "dallas", "tx", "usa");
+VALUES ("1", "123" ,"45000", "1234 street", "567 blvd", "dallas", "tx", "usa")
+
+INSERT INTO dept (ID, name1, location1 )
+VALUES ("1", "Tina" ,"houston" );
+VALUES ("1", "will" ,"dallas" );
+VALUES ("1", "john" ,"austin" )
+
+
+
+
+-- out of time, wanted to complete 
+
